9a45408c4883e25b74b202a727e8b7797f8358e3
Stephen Fisher <127833178+StephenAFisher@users.noreply.github.com>
2023-09-20 18:10:08 -0700

    Update aiBoostedOrderProcessor.py with OpenAI changes for command line

--- src/aiBoostedOrderProcessor.py
+++ src/aiBoostedOrderProcessor.py
@@ -2,12 +2,10 @@ import openai
 import pandas as pd
 import json
 import urllib.parse
+import argparse
 
-# Initialization
-openai.api_key = 'YOUR_OPENAI_API_KEY'
-base_amazon_url = "https://www.amazon.com/s?k="
-
-def process_order(email, order_text):
+def process_order(api_key, email, order_text):
+    openai.api_key = api_key
     prompt = f"From customer {email}, the order request is: '{order_text}'. Process this order and extract required structured data."
     response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=200)
     return response.choices[0].text.strip()
@@ -15,30 +13,40 @@ def process_order(email, order_text):
 def amazon_uri(product_name):
     return base_amazon_url + urllib.parse.quote(product_name)
 
-df = pd.read_excel("path_to_file.xlsx")  # Replace 'path_to_file.xlsx' with your file path.
+def main():
+    parser = argparse.ArgumentParser(description="Process orders from Excel using OpenAI GPT API.")
+    parser.add_argument("api_key", help="Your OpenAI API key")
+    parser.add_argument("excel_path", help="Path to the Excel file with orders")
+    args = parser.parse_args()
 
-all_orders = []
+    base_amazon_url = "https://www.amazon.com/s?k="
 
-for index, row in df.iterrows():
-    print(f"Processing Order from Customer: {row['email']}")
-    response = process_order(row['email'], row['order'])
-    structured_data = json.loads(response)
-    print(structured_data)
-    all_orders.append(structured_data)
+    df = pd.read_excel(args.excel_path)
 
-    if 'ProductName' in structured_data:
-        print(f"Amazon Search URL: {amazon_uri(structured_data['ProductName'])}")
+    all_orders = []
 
-# Submit a ChatGPT prompt for summary report
-summary_prompt = f"Here is the summary of all orders: {json.dumps(all_orders)}. Provide a summary report."
-summary_response = openai.Completion.create(engine="text-davinci-003", prompt=summary_prompt, max_tokens=200)
-print(summary_response.choices[0].text.strip())
+    for index, row in df.iterrows():
+        print(f"Processing Order from Customer: {row['email']}")
+        response = process_order(args.api_key, row['email'], row['order'])
+        structured_data = json.loads(response)
+        print(structured_data)
+        all_orders.append(structured_data)
 
-# Individual customer report
-unique_emails = df['email'].unique()
-for email in unique_emails:
-    individual_orders = [order for order in all_orders if order['Customer email'] == email]
-    individual_prompt = f"Here are the orders for {email}: {json.dumps(individual_orders)}. Provide a report based on the customer's orders."
-    individual_response = openai.Completion.create(engine="text-davinci-003", prompt=individual_prompt, max_tokens=200)
-    print(individual_response.choices[0].text.strip())
+        if 'ProductName' in structured_data:
+            print(f"Amazon Search URL: {amazon_uri(structured_data['ProductName'])}")
 
+    # Submit a ChatGPT prompt for summary report
+    summary_prompt = f"Here is the summary of all orders: {json.dumps(all_orders)}. Provide a summary report."
+    summary_response = openai.Completion.create(engine="text-davinci-003", prompt=summary_prompt, max_tokens=200)
+    print(summary_response.choices[0].text.strip())
+
+    # Individual customer report
+    unique_emails = df['email'].unique()
+    for email in unique_emails:
+        individual_orders = [order for order in all_orders if order['Customer email'] == email]
+        individual_prompt = f"Here are the orders for {email}: {json.dumps(individual_orders)}. Provide a report based on the customer's orders."
+        individual_response = openai.Completion.create(engine="text-davinci-003", prompt=individual_prompt, max_tokens=200)
+        print(individual_response.choices[0].text.strip())
+
+if __name__ == "__main__":
+    main()
