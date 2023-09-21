f407fa90c7fc0b22b75c78697830f83dc5a00018
Stephen Fisher <127833178+StephenAFisher@users.noreply.github.com>
2023-09-21 02:39:37 -0700

    Fixes for conversion to function-based API

--- src/aiBoostedOrderProcessor.py
+++ src/aiBoostedOrderProcessor.py
@@ -1,49 +1,68 @@
 import warnings
-from urllib3.exceptions import NotOpenSSLWarning
+from urllib3.exceptions import HTTPWarning
 import openai
 import pandas as pd
 import json
 import urllib.parse
 import argparse
-import requests
 
-warnings.simplefilter('ignore', category=NotOpenSSLWarning)
+warnings.simplefilter('ignore', category=HTTPWarning)
 
-def call_openai_api(prompt, openai_key):
-    headers = {
-        "Authorization": f"Bearer {openai_key}",
-        "Content-Type": "application/json"
-    }
-    
+def call_openai_api(prompt):
     data = {
-        "model": "get-4-0613",
-        "prompt": prompt,
-        "function_call": {
-            "name": "process_order",
-            "parameters": {
-                "customer_email": "",
-                "product_name": "",
-                "quantity": 0,
-                "ManualProcessingRequired": False,
-                "CustomerSupportRequired": ""
-            }
-        },
-        "max_tokens": 1000
+        "model": "gpt-4-0613",
+        'max_tokens': 1000,
+        "messages": [
+            {"role": "user", "content": prompt},
+        ],
     }
-    
-    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=data)
-    response.raise_for_status()
-    response_text = response.json()["choices"][0]["text"].strip()
 
-    try:
-        return json.loads(response_text)
-    except json.JSONDecodeError:
-        print(f"Error: Could not decode the response into JSON. Response was: {response_text}")
-        return None
+    # Assuming you have set the API key and imported the OpenAI library
+    response = openai.ChatCompletion.create(**data)
+    return response.choices[0].message['content']
 
-def process_order(openai_key, email, order_request):
+
+def call_openai_function_api(prompt, function_name, function_schema):
+    # Assuming function_name is a user-defined function you're trying to "simulate"
+    system_instruction = f"Use the call_{function_name} function to process the following input:"
+
+    data = {
+        "model": "gpt-4-0613",
+        "functions": [
+            function_schema
+        ],
+        'function_call': {
+            'name': 'call_' + function_name,
+        },
+        'max_tokens': 1000,
+        "messages": [
+            {"role": "system", "content": system_instruction},
+            {"role": "user", "content": prompt},
+        ],
+    }
+
+    # Assuming you have set the API key and imported the OpenAI library
+    response = openai.ChatCompletion.create(**data)
+    return response.choices[0].message['function_call']['arguments']
+
+
+def process_order(email, order_request):
     prompt = f"Convert the order request: '{order_request}' from '{email}' into JSON."
-    return call_openai_api(prompt, openai_key)
+    function_schema = {
+            "name": "call_" + "process_order",
+            "description": "",
+            "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "customer_email": { "type": "string", "description": "Well formed email of the customer or empty if unknown"},
+                        "product_name": { "type": "string", "description": "Short product description or empty if unknown"},
+                        "quantity": { "type": "integer", "description": "The count of the product"},
+                        "ManualProcessingRequired": { "type": "boolean", "description": "True if the order request does not have complete information"},
+                        "CustomerSupportRequired": { "type": "string", "description": "Support questions from the customer if there are any, or empty if none"}
+                    }
+            }
+        }
+    return call_openai_function_api(prompt, "process_order", function_schema)
 
 def amazon_uri(product_name):
     return f"https://www.amazon.com/s?k={urllib.parse.quote(product_name)}"
@@ -57,14 +76,18 @@ def main():
     df = pd.read_excel(args.excel_path)
     all_orders = []
 
+    openai.api_key = args.api_key
+
     for index, row in df.iterrows():
-        response = process_order(args.api_key, row['email'], row['order'])
+        response = process_order(row['email'], row['order'])
         try:
             structured_data = json.loads(response)
             
             if not structured_data or 'product_name' not in structured_data:
                 print(f"Unexpected response for order {row['order']} from {row['email']}: {response}")
                 continue
+
+            print(f"Order Processed: {str(structured_data)}")
             
             all_orders.append(structured_data)
 
@@ -77,16 +100,16 @@ def main():
 
     # Submit a ChatGPT prompt for summary report
     summary_prompt = f"Here is the summary of all orders: {json.dumps(all_orders)}. Provide a summary report."
-    summary_response = openai.Completion.create(engine="text-davinci-003", prompt=summary_prompt, max_tokens=200)
-    print(summary_response.choices[0].text.strip())
+    summary_response = call_openai_api(summary_prompt)
+    print(summary_response)
 
     # Individual customer report
     unique_emails = df['email'].unique()
     for email in unique_emails:
         individual_orders = [order for order in all_orders if order['Customer email'] == email]
         individual_prompt = f"Here are the orders for {email}: {json.dumps(individual_orders)}. Provide a report based on the customer's orders."
-        individual_response = openai.Completion.create(engine="text-davinci-003", prompt=individual_prompt, max_tokens=200)
-        print(individual_response.choices[0].text.strip())
+        individual_response = call_openai_api(individual_prompt)
+        print(individual_response)
 
 if __name__ == "__main__":
     main()
